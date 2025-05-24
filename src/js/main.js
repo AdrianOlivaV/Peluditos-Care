cdd7cc4b2a83b96fdb2a3b2e53e7aa76b208c19f
AdrianOlivaV <jao.vejar@gmail.com>
2025-05-24 01:17:19 -0700

    feat: importando header en pagina principal

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import footer from "../modules/footer/footer.js";
-
+import header from"../modules/header/header.js";
+header(document.getElementById("header"));
 footer(document.getElementById("footer"));
-=======
-import header from '../modules/header/header.js'
-
-header(document.getElementById('header'));
->>>>>>> header
