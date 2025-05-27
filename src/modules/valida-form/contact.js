38ef2cc04aea20a734e08900319c498830f38395
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-05-27 10:22:03 -0600

    feat: se crean archivos para validación del formulario

---
+++ src/modules/valida-form/contact.js
@@ -0,0 +1,24 @@
+import header from "../../modules/header/header.js";
+import footer from "../../modules/footer/footer.js"
+import { leerInputsFormulario } from "./leerInputsFormulario.js";
+
+
+header(document.getElementById("header"));
+footer(document.getElementById("footer"));
+
+//---------------------------------------------
+
+const registerFrom = document.getElementById("form-Contacto");
+
+registerFrom.addEventListener("submit", async (e) => {
+    e.preventDefault(); // evita que se envíe el formulario
+    console.log("Manejo del formulario");
+
+    // Obtener los datos del formulario.
+    const newConsult = leerInputsFormulario(registerFrom);
+    // const validatedUser = isUserValid(newConsult);
+    console.log(newConsult);
+
+    
+});
+
---
+++ src/modules/valida-form/leerInputsFormulario.js
@@ -0,0 +1,14 @@
+
+
+const leerInputsFormulario = ( formulario )=>{
+    const newConsult = {
+        nombre : formulario.elements["nombre"].value,
+        apellido : formulario.elements["apellido"].value,
+        celular : formulario.elements["telefono"].value,
+        motivo : formulario.elements["motivo"].value,
+        descripcion : formulario.elements["descripcion"].value
+    }
+    console.table(newConsult);
+    return newConsult;
+}
+export { leerInputsFormulario };
