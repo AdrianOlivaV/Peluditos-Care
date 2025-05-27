fe2431fe8c2147518dd8b7b6f146aaff02b59b12
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-05-27 11:08:04 -0600

    feat: se crea la validación del nombre y alerts para que el usuario sepa que datos modificar en caso de error

---
+++ src/js/validations/validateName.js
@@ -0,0 +1,21 @@
+
+
+const validateName = (nombre) => {
+    const result = {
+        isValid: true,
+        errors: []
+    };
+
+    if( nombre === "" ){
+        result.isValid = false;
+        alert("El nombre no debe estar vacío");
+    } else if (nombre.length < 3 ) {
+        result.isValid = false;
+        alert("El nombre debe tener al menos 3 caracteres");
+    }
+
+    return result;
+
+}
+
+export { validateName };
