8096f53636381bfe25dcf84966fdb204e87cc9d3
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-05-29 09:33:55 -0600

    feat: se importan header y footer

---
+++ src/pages/owner profile/ownerProfile.js
@@ -0,0 +1,5 @@
+import insertMainHeader from "../../modules/header/header.js"
+import insertMainFooter from "../../modules/footer/footer.js";
+
+insertMainHeader(document.getElementById("header"));
+insertMainFooter(document.getElementById("footer"))
