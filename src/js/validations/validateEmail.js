214a5065d4596820d5ae53525b981cd9c32d104e
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-05-29 17:18:26 -0600

    feat: se agrega funcion que valide el correo electronico y su estructura

---
+++ src/js/validations/validateEmail.js
@@ -0,0 +1,31 @@
+
+const validateEmail = (correo) => {
+    const result = {
+        isValid: true,
+        errors: []
+    };
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (correo === "") {
+        result.isValid = false;
+        result.errors.push("Debes de incluir un correo electrónico.");
+        alert("Debes de incluir un correo electrónico.");
+    } else if (!correo.includes("@")) {
+        result.isValid = false;
+        result.errors.push("El correo debe incluir un '@'.");
+        alert("El correo debe incluir un '@'.");
+    } else if (!correo.includes(".")) {
+        result.isValid = false;
+        result.errors.push("El correo debe incluir un dominio con '.'");
+        alert("El correo debe incluir un dominio con '.'.");
+    } else if (!emailRegex.test(correo)) {
+        result.isValid = false;
+        result.errors.push("Correo electrónico no válido.");
+        alert("Correo electrónico no válido.");
+    }
+
+    return result;
+}
+
+export { validateEmail };
