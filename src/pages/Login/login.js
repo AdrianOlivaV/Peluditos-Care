85985699a563fca87d916e9a7e167209c9643f21
Mon Buelna <montserratbuelna.lni@gmail.com>
2025-05-30 19:09:20 -0600

    feat: importacion de header

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,3 +1,5 @@
+import insertMainHeader from "../../modules/header/header.js";
 import footer from "../../modules/footer/footer.js";
 
+insertMainHeader(document.getElementById("header"));
 footer(document.getElementById("footer"));
