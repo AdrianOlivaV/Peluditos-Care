389da616751f3fc9bd961eef873550aade5b6dc1
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-06-04 23:29:39 -0600

    feat: archivo que contiene todos los testimonios de la página testimonies.html

---
+++ src/pages/testimonies/testimoniesList.js
@@ -0,0 +1,74 @@
+const testimoniesList = [
+  {
+    name: 'Fabian',
+    location: 'CDMX, Iztapalapa',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsa, praesentium harum ad, facilis veniam dolorem qui consequatur id, cumque nam natus pariatur ratione dicta deleniti asperiores sequi maiores quisquam.',
+    type_service: 'Paseo Matutino',
+    img: 'https://petsmileusa.com/cdn/shop/articles/healthy-pet-activities.png?v=1619548965'
+  },
+  {
+    name: 'Mauricio',
+    location: 'GDL, Zapopan',
+    comment: 'Aliquam ipsa, praesentium harum ad, facilis veniam dolorem qui consequatur id, cumque nam natus pariatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dicta deleniti asperiores sequi maiores quisquam.',
+    type_service: 'Hospedaje',
+    img: 'https://www.lifeextension.com/-/media/lifeextension/wellness/pet-health/dog-food-additives/dog-food-additives_lead.jpg?rev=4da149edf5e14e8cba0abfaafd79c202&h=400&w=1080&la=en&hash=56F50A33CE1CFB3026060886722774A8'
+  },
+  {
+    name: 'Karen',
+    location: 'CDMX, Roma',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsa, praesentium harum ad, facilis veniam dolorem qui consequatur id, cumque nam natus pariatur ratione dicta deleniti asperiores sequi maiores quisquam. Aliquam ipsa, praesentium harum ad, facilis veniam dolorem qui consequatur id, cumque nam natus pariatur.',
+    type_service: 'Guarderia',
+    img: 'https://blog.nationwide.com/wp-content/uploads/fly-images/30440/AdobeStock_326439311-1130x750-ct.jpg'
+  },
+  {
+    name: 'Mariana',
+    location: 'GDL, La Minerva',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsa, praesentium harum ad, facilis veniam dolorem qui consequatur id, cumque nam natus pariatur ratione dicta deleniti asperiores sequi maiores quisquam.',
+    type_service: 'Paseo Nocturno',
+    img: 'https://www.heart.org/-/media/healthy-living-images/healthy-lifestyle/pets/dogs-woman-running-beach.jpg'
+  },
+  {
+    name: 'Andres',
+    location: 'CDMX, Azcapotzalco',
+    comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam iste architecto incidunt? Neque voluptatem totam accusantium excepturi dignissimos minus facilis eveniet tempore, adipisci ipsam repellendus, nobis hic distinctio sapiente nemo.',
+    type_service: 'Visitas a domicilio',
+    img: 'https://images.prismic.io/trustedhousesitters/Zip4L_Pdc1huK0Hx_reptilepetsitter.png?auto=format%2Ccompress&rect=0%2C0%2C1920%2C800&w=1920&h=800'
+  },
+  {
+    name: 'Tania',
+    location: 'GDL, Americana',
+    comment: 'Quam iste architecto incidunt? Neque voluptatem totam accusantium excepturi dignissimos minus facilis eveniet tempore, adipisci ipsam repellendus, nobis hic distinctio sapiente nemo.',
+    type_service: 'Visita a domicilio',
+    img: 'https://images.unsplash.com/photo-1620126661556-5c4efae896fe?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    name: 'Oliver',
+    location: 'CDMX, Reforma',
+    comment: 'Adipisci ipsam repellendus, nobis hic distinctio sapiente nemo.',
+    type_service: 'Baño',
+    img: 'https://plus.unsplash.com/premium_photo-1663036405014-3b4f2713633c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    name: 'Nancy',
+    location: 'GDL, Miraflores',
+    comment: 'Praesentium, eius error nulla expedita ad ex nobis qui consectetur suscipit quibusdam cupiditate reiciendis, impedit illo nisi veniam sint exercitationem!',
+    type_service: 'Paseo Matutino',
+    img: 'https://www.vets4pets.com/siteassets/species/rabbit/rabbit-with-girl-inside-house.jpg'
+  },
+  {
+    name: 'Kary',
+    location: 'CDMX, Buenavista',
+    comment: 'Adipisci ipsam repellendus, nobis hic distinctio sapiente nemo. Praesentium, eius error nulla expedita ad ex nobis qui consectetur suscipit quibusdam cupiditate reiciendis, impedit illo nisi veniam sint exercitationem!',
+    type_service: 'Baño',
+    img: 'https://www.arlboston.org/wp-content/uploads/2025/03/wellness-waggin-staff-scaled.jpg'
+  },
+  {
+    name: 'Enrique',
+    location: 'GDL, Huentitán El Alto',
+    comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsa, praesentium harum ad, facilis veniam dolorem qui consequatur id, cumque nam natus pariatur ratione dicta deleniti asperiores sequi maiores quisquam.',
+    type_service: 'Guarderia',
+    img: 'https://hudspethanimalhospital.com/wp-content/uploads/2024/12/HAH-membership-club.png'
+  }
+];
+
+export default testimoniesList;
