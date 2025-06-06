0e7fb21f2fee46e278d496f11b4129a88a1032f8
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-06-05 19:46:46 -0600

    feat: se enlaza el evento de submit con las funciones de validación al momento de querer ingresar al sitio

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,5 +1,23 @@
 import insertMainHeader from "../../modules/header/header.js";
 import footer from "../../modules/footer/footer.js";
+import { leerInputsLogin } from "./inputsLogin.js";
+import { isUserValidLogin } from "../../js/validationsLogin/isValidUserLogin.js";
 
 insertMainHeader(document.getElementById("header"));
 footer(document.getElementById("footer"));
+
+//---------------------------------------------
+
+const loginForm = document.getElementById("form-login");
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault(); // evita que se envíe el formulario
+    console.log("Manejo del formulario");
+
+    // Obtener los datos del formulario.
+    const newConsult = leerInputsLogin(loginForm);
+    const validatedUser = isUserValidLogin(newConsult);
+     console.log(validatedUser);
+
+    
+});
