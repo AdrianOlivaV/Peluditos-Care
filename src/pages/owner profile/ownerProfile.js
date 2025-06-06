8ee44c27a8173dfa3ab7789bae1dbddeff39b225
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-06-05 20:06:45 -0600

    docs: se desecha la ruta de los archivos por cambio en el nombre de la carpeta

--- src/pages/owner profile/ownerProfile.js
+++
@@ -1,5 +0,0 @@
-import insertMainHeader from "../../modules/header/header.js"
-import insertMainFooter from "../../modules/footer/footer.js";
-
-insertMainHeader(document.getElementById("header"));
-insertMainFooter(document.getElementById("footer"))
