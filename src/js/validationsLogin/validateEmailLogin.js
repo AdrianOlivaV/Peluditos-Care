e5d27700db7583333601e66d9a0daa5a4b951d03
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-06-15 19:44:14 -0600

    docs: se eliminan los alerts que aparecen en la ventana, ya que ahora aparecen los alerts en el formulario

--- src/js/validationsLogin/validateEmailLogin.js
+++ src/js/validationsLogin/validateEmailLogin.js
@@ -11,19 +11,15 @@ const validateEmailLogin = (email) => {
     if (email === "") {
         result.isValid = false;
         result.errors.push("Debes de incluir un correo electrónico.");
-        alert("Debes de incluir un correo electrónico.");
     } else if (!email.includes("@")) {
         result.isValid = false;
         result.errors.push("El correo debe incluir un '@'.");
-        alert("El correo debe incluir un '@'.");
     } else if (!email.includes(".")) {
         result.isValid = false;
         result.errors.push("El correo debe incluir un dominio con '.'");
-        alert("El correo debe incluir un dominio con '.'.");
     } else if (!emailRegex.test(email)) {
         result.isValid = false;
         result.errors.push("Correo electrónico no válido.");
-        alert("Correo electrónico no válido.");
     }
 
     return result;
