1523e028ed3b0643231b478f5d159ad2089215fb
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-06-15 18:27:52 -0600

    docs: se homolga código al ingles, cambiando correo por email

--- src/js/validationsLogin/validateEmailLogin.js
+++ src/js/validationsLogin/validateEmailLogin.js
@@ -1,6 +1,6 @@
 
 
-const validateEmailLogin = (correo) => {
+const validateEmailLogin = (email) => {
     const result = {
         isValid: true,
         errors: []
@@ -8,19 +8,19 @@ const validateEmailLogin = (correo) => {
 
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-    if (correo === "") {
+    if (email === "") {
         result.isValid = false;
         result.errors.push("Debes de incluir un correo electrónico.");
         alert("Debes de incluir un correo electrónico.");
-    } else if (!correo.includes("@")) {
+    } else if (!email.includes("@")) {
         result.isValid = false;
         result.errors.push("El correo debe incluir un '@'.");
         alert("El correo debe incluir un '@'.");
-    } else if (!correo.includes(".")) {
+    } else if (!email.includes(".")) {
         result.isValid = false;
         result.errors.push("El correo debe incluir un dominio con '.'");
         alert("El correo debe incluir un dominio con '.'.");
-    } else if (!emailRegex.test(correo)) {
+    } else if (!emailRegex.test(email)) {
         result.isValid = false;
         result.errors.push("Correo electrónico no válido.");
         alert("Correo electrónico no válido.");
