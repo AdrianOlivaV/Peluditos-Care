0bfe1b26ecffc95b6c51214812035aea8aa36d98
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-06-15 18:27:42 -0600

    docs: se homolga código al ingles, cambiando correo por email

--- src/js/validationsLogin/isValidUserLogin.js
+++ src/js/validationsLogin/isValidUserLogin.js
@@ -6,22 +6,38 @@ const isUserValidLogin = (user) => {
         isValid: true,
         errors: []
     };
-    
+
     //validacion del correo ingresado en el Login
-    const emailValidate = validateEmailLogin(user.correo);
-    if( emailValidate.isValid === false){
+    const emailValidate = validateEmailLogin(user.email);
+    if (emailValidate.isValid === false) {
         result.isValid = false;
         result.errors.push(emailValidate.errors);
     }
 
     //validacion de la contraseña
     const passwordValidate = validatePasswordLogin(user.password);
-    if( passwordValidate.isValid === false){
+    if (passwordValidate.isValid === false) {
         result.isValid = false;
         result.errors.push(passwordValidate.errors);
     }
 
+    // Validación contra usuario
+    const userDB = {
+        email: "usuario@ejemplo.com",
+        password: "12345678"
+    };
 
+    if (result.isValid) {
+        if (user.correo !== userDB.correo || user.password !== userDB.password) {
+            result.isValid = false;
+            result.errors.push("Usuario o contraseña incorrectos.");
+        }
+    }
+
+    return result;
+
+    return email === userDB.email && password === userDB.password;
 }
 
-export {isUserValidLogin};
+
+export { isUserValidLogin };
