7675a8cf6c03c3abcacb8b597402de71cba48f56
Vero_Vazquez <vazquezrojas9803@gmail.com>
2025-06-15 19:43:13 -0600

    feat: se reemplazan correo y password predefinido para que obtenga los usuarios precargados en la local storage.
    se añade btoa, para códificar correo y contraseña, se añade console.table para verificar que los datos van códificados

--- src/js/validationsLogin/isValidUserLogin.js
+++ src/js/validationsLogin/isValidUserLogin.js
@@ -21,22 +21,23 @@ const isUserValidLogin = (user) => {
         result.errors.push(passwordValidate.errors);
     }
 
-    // Validación contra usuario
-    const userDB = {
-        email: "usuario@ejemplo.com",
-        password: "12345678"
-    };
+    // !!Importante: btoa es bueno para pruebas, más no en producción
+    // Validación contra usuario guardado en localStorage
+    const storedUser = JSON.parse(localStorage.getItem("user"));
 
     if (result.isValid) {
-        if (user.correo !== userDB.correo || user.password !== userDB.password) {
+        const inputEmail = btoa(user.email);
+        const inputPassword = btoa(user.password);
+
+        if (!storedUser || inputEmail !== storedUser.email || inputPassword !== storedUser.password) {
             result.isValid = false;
             result.errors.push("Usuario o contraseña incorrectos.");
         }
+        console.table("El correo es :" + inputEmail + 
+                        "\n La contraseña es :" + inputPassword); //solo para probar
     }
 
     return result;
-
-    return email === userDB.email && password === userDB.password;
 }
 
 
